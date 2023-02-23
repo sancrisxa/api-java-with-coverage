dadd45333c3af54431ee63ff835e8262fc7b9dfd
Samuel Xavier <Samuel.CristianoSilvaXavier-ext@ab-Inbev.com>
2023-02-23 02:36:04 -0300

    first commit

---
+++ src/test/java/br/com/sancrisxa/api/ApiApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.sancrisxa.api;
+
+import br.com.dicasdeumdev.api.ApiApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void main() {
+        ApiApplication.main(new String[] {});
+    }
+
+}
